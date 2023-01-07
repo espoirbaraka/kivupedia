f2753ffd648a32572cb1c92f6f172ef6f87036bb
espoirbaraka <esbarakabigega@example.com>
2023-01-07 16:05:07 +0100

    Detail livre

---
+++ container/read_book.php
@@ -0,0 +1,24 @@
+<?php
+$book = $_GET['book'];
+
+$sql = "SELECT * FROM t_livre
+                WHERE book_slug = '$book'";
+$req = $app->fetch($sql);
+?>
+
+
+<div class="single-channel">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index"><i class="fa fa-home"></i> Acceuil</a></li>
+                        <li class="breadcrumb-item active" aria-current="page"><a href="index"><i class="fa fa-arrow-left"></i> <a href="<?php echo $_SERVER["HTTP_REFERER"]; ?>">Retour</a></a></li>
+                    </ol>
+                </nav>
+                <iframe src="superadmin/fichier/<?php echo $req['Fichier_livre'] ?>" style="width: 100%; height: 100vh;"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
