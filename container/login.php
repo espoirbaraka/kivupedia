7bbd5cf7008ce2a77fb1b44563acba0c7c917c44
espoirbaraka <esbarakabigega@example.com>
2023-01-08 21:38:56 +0100

    admin panel

---
+++ container/login.php
@@ -0,0 +1,45 @@
+<div class="single-channel">
+    <div class="container">
+
+        <div class="single-channel">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+
+                        <form class="form-signin">
+<!--                            <div class="text-center mb-4">-->
+<!--                                <img class="mb-4" src="superadmin/img/user.png" alt="" width="72" height="72">-->
+<!--                                <h1 class="h3 mb-3 font-weight-normal">Se connecter</h1>-->
+<!--                                -->
+<!--                            </div>-->
+
+                            <div class="form-label-group">
+                                <input type="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required autofocus>
+                                <label for="inputEmail">Adresse mail</label>
+                            </div>
+
+                            <div class="form-label-group">
+                                <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+                                <label for="inputPassword">Mot de passe</label>
+                            </div>
+
+                            <div class="checkbox mb-3">
+                                <label>
+                                    <input type="checkbox" value="remember-me"> Se souvenir de moi
+                                </label>
+                            </div>
+                            <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
+
+                        </form>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
--- includes/header.php
+++ includes/header.php
@@ -13,15 +13,22 @@
                 <div class="col-md-9 col-sm-9 nav-wrapper">
 
                     <nav>
-                        <ul class="sf-menu" id="menu">
-                            <li><a href="home" class="navv">Acceuil</a></li>
-                            <li><a href="article" class="navv">TFC/Memoire</a></li>
-                            <li><a href="courses" class="navv">Cours</a></li>
-                            <li><a href="item" class="navv">ITEM</a></li>
-                            <li><a href="about-us" class="navv">A propos</a></li>
 
-<!--                            <button class="btn btn-lg" style=" background-color: #ea5b0e; margin-left: 40px; margin-top: 25px;"> Se connecter</button>-->
-                        </ul>
+                            <div style="float: left;">
+                                <ul class="sf-menu" id="menu">
+                                    <li><a href="home" class="navv">Acceuil</a></li>
+                                    <li><a href="article" class="navv">TFC/Memoire</a></li>
+                                    <li><a href="courses" class="navv">Cours</a></li>
+                                    <li><a href="item" class="navv">ITEM</a></li>
+                                    <li><a href="about-us" class="navv">A propos</a></li>
+                                </ul>
+                            </div>
+                            <div style="float: right;" id="menu1">
+                                <ul class="sf-menu">
+                                    <li><a href="login" style="border: 2px solid #b70c0c; color: #b70c0c;">Se connecter</a></li>
+                                </ul>
+                            </div>
+
 
                     </nav>
 
