e326fa6fd8712002029959895ca28eebd68d6c6c
espoirbaraka <esbarakabigega@example.com>
2023-01-24 16:46:20 +0100

    ask question

--- superadmin/content/askquestion.php
+++ superadmin/content/askquestion.php
@@ -40,111 +40,106 @@ $day = $_GET['day'];
         <div class="row">
             <div class="col-xs-12">
 
-                <?php
-                for($j=1; $j<=5; $j++){
-                   ?>
-                    <div class="box" style="overflow: auto;">
+                <form>
+                    <?php
+                    for($j=1; $j<=5; $j++){
+                        ?>
+                        <div class="box" >
 
 
-                        <div class="box-header with-border">
-                            <h4 class="box-title" style="font-weight: bold">QUESTION <?php echo $j; ?></h4>
-                        </div>
+                            <div class="box-header with-border">
+                                <h4 class="box-title" style="font-weight: bold">QUESTION <?php echo $j; ?></h4>
+                            </div>
 
 
-                        <div class="box-body">
+                            <div class="box-body">
 
-                            <div class="row">
+                                <div class="row">
 
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label>Question <span style="color: red;">*</span></label>
-                                        <textarea class="textarea" name="description" style="width: 100%; height: 80px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label>Question <span style="color: red;">*</span></label>
+                                            <textarea class="textarea" name="description" style="width: 100%; height: 80px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
 
                                         </textarea>
+                                        </div>
+
                                     </div>
 
                                 </div>
 
-                            </div>
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label>Réponse 1 <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" name="reponse" required>
+                                        </div>
 
-                            <div class="row">
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label>Réponse 1 <span style="color: red;">*</span></label>
-                                        <input type="text" class="form-control" name="reponse" required>
                                     </div>
 
-                                </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label>Réponse 2 <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" name="reponse" required>
+                                        </div>
 
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label>Réponse 2 <span style="color: red;">*</span></label>
-                                        <input type="text" class="form-control" name="reponse" required>
                                     </div>
 
-                                </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label>Réponse 3 <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" name="reponse" required>
+                                        </div>
 
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label>Réponse 3 <span style="color: red;">*</span></label>
-                                        <input type="text" class="form-control" name="reponse" required>
                                     </div>
 
-                                </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label>Réponse 4 <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" name="reponse" required>
+                                        </div>
 
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label>Réponse 4 <span style="color: red;">*</span></label>
-                                        <input type="text" class="form-control" name="reponse" required>
                                     </div>
 
-                                </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label>Réponse 5 <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" name="reponse" required>
+                                        </div>
 
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label>Réponse 5 <span style="color: red;">*</span></label>
-                                        <input type="text" class="form-control" name="reponse" required>
                                     </div>
 
-                                </div>
-
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label>Cochez la bonne reponse <span style="color: red;">*</span></label>
-                                        <select class="form-control" name="bn_reponse" required>
-                                            <option>-- Choissessez une assertion --</option>
-                                            <?php
-                                            for($i=1; $i<=5; $i++)
-                                            {
-                                                ?>
-                                                <option value="<?php echo $i; ?>">Assertion <?php echo $i; ?></option>
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label>Cochez la bonne reponse <span style="color: red;">*</span></label>
+                                            <select class="form-control" name="bn_reponse" required>
+                                                <option>-- Choissessez une assertion --</option>
                                                 <?php
-                                            }
-                                            ?>
+                                                for($i=1; $i<=5; $i++)
+                                                {
+                                                    ?>
+                                                    <option value="<?php echo $i; ?>">Assertion <?php echo $i; ?></option>
+                                                    <?php
+                                                }
+                                                ?>
 
-                                        </select>
+                                            </select>
+                                        </div>
+
                                     </div>
 
                                 </div>
 
+
+
+
                             </div>
-
-
-
 
                         </div>
-
-                    </div>
-                <?php
-                }
-                ?>
-
-
-
-
-
-
-
+                        <?php
+                    }
+                    ?>
+                </form>
 
                 <!-- /.box -->
             </div>
